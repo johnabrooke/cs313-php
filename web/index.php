add251294bf673ac7b6b1ca85f4a37a7fc985693
John Brooke <johnabrooke@gmail.com>
2021-01-18 07:50:37 -0700

    add files with initial code for Prove02 Homepage

--- web/index.php
+++ web/index.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>John's Homepage</title>
+  <link rel="stylesheet" href="styles.css">
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <?php include("navbar.php"); ?>
+</head>
+<body>
+  <div class="grid-container">
+    <div class="grid-item">
+      <img src="img/2Tim1_7_power_of_love.jpeg" 
+           alt="Quote from Elder Neil L. Andersen. Some will say, You don't understand my situation... 
+                I testify there is One who does understand.">
+    </div>
+    <div class="grid-item">
+      <img src="img/light_of_the_world_jesus_christ_medium.jpeg" alt="Jesus Christ headshot">
+    </div>
+    <div class="grid-item">
+      <img src="img/nelson_conference_oct_2019_quote.png" 
+           alt="Quote from President Russell M. Nelson. The restored Gospel of Jesus Christ will bring hope and joy.">
+    </div>
+  </div>
+  
+</body>
+</html>
 <?php
 
   phpinfo();
---
+++ web/navbar.php
@@ -0,0 +1,23 @@
+<head>
+    <link rel="stylesheet" href="navStyles.css">
+</head>
+<nav role="navigation">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#" aria-haspopup="true">Assignments</a>
+            <ul class="dropdown" aria-label="submenu">
+                <li><a href="#">Placeholder for Shopping Cart</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+                <li><a href="#">Placeholder</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Team Activities</a></li>
+    </ul>
+</nav>
