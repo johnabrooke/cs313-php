92c68154d631d297607b58daee76c97262b0ccd7
John Brooke <johnabrooke@gmail.com>
2021-01-24 01:23:58 -0700

    add files for prove03 shopping cart assignment and beginning code

--- web/prove03-cart/index.php
+++ web/prove03-cart/index.php
@@ -1,13 +1,19 @@
 <?php
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping Cart</title>
+    <title>Shopping Cart for School Assignment (not a real shopping site)</title>
 </head>
 <body>
-    
+    <div>
+        <div>
+            <img src="imgs/Education_for_Better_Work.png" alt="Front cover for Education for Better Work book."/>
+            <button>Add to Cart</button>
+        </div>
+    </div>
 </body>
 </html>
